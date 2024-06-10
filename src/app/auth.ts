ea194086264181408a106cfe6c7b2ab348caf644
evadshell <evadshell69@gmail.com>
2024-06-11 02:10:18 +0530

    basic auth added FINALLYgit add .!

---
+++ src/app/auth.ts
@@ -0,0 +1,47 @@
+
+// import NextAuth from "next-auth"
+// import GitHub from "next-auth/providers/github"
+// import GoogleProvider from "next-auth/providers/google";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter"
+// import clientPromise from "./lib/db"
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [GitHub,GoogleProvider],adapter: MongoDBAdapter(clientPromise),callbacks: {
+//     async session({ session, user }) {
+//       session.userId = user.id;
+//       return session;
+//     },
+//   },
+//   debug: true, 
+// })
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "./lib/db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    async session({ session, user }) {
+      session.userId = user.id;
+      return session;
+    },
+    async signIn({ user, account, profile, email, credentials }) {
+      if (!user.username) {
+        user.username = user?.email?.split("@")[0];
+      }
+      return true;
+    },
+  },
+  debug: true,
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,40 @@
-export default function Home(){
-  return <main>
-    Hello World
-  </main>
+import Image from "next/image";
+import { signOut, signIn, auth } from "./auth";
+export default async function Home() {
+  const session = await auth();
+  console.log(session?.user);
+  return (
+    <>
+      {session?.user ? (
+        <>
+          <h1>{session?.user?.name}</h1>
+          <Image
+            src={session?.user?.image || ""}
+            width={100}
+            height={100}
+            alt="User Image"
+          />{" "}
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <button type="submit">Sign Out</button>
+          </form>
+        </>
+      ) : (
+        <>
+          <form
+            action={async () => {
+              "use server";
+              await signIn(); //dont put anything in bracket so that every option can be shown
+            }}
+          >
+            <button type="submit">Signin</button>
+          </form>
+        </>
+      )}
+    </>
+  );
 }
