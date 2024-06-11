a0b297eea9db32ea6bf494e42202fc3d583787ca
evadshell <evadshell69@gmail.com>
2024-06-11 22:03:57 +0530

    lol

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import { redirect } from 'next/navigation'
 import { auth } from "../auth"
-
+import { use, useEffect } from "react";
 export default async function Dashboard(){
     const session = await auth();
+    const checkAuth =()=>{
+if(!session){
+redirect("/")
+}
+}
+checkAuth();
+
     return (<>
     <h1>Dashboard
         {session?.user?.id}
