a042d846790fa169261974e0f12d1e5ff72b947f
evadshell <evadshell69@gmail.com>
2024-06-18 04:34:24 +0530

    minor change

--- src/app/sniphistory/[id]/page.tsx
+++ src/app/sniphistory/[id]/page.tsx
@@ -3,7 +3,6 @@ import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Code } from '@chakra-ui/react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
